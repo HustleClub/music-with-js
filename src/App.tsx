0aa84075d8eddb1ef686d8e8f75ab3c0b5cd5399
rjan <daaka@pop-os.localdomain>
2022-12-09 18:44:08 +0545

    playing a sound

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-
+import * as Tone from 'tone'
 function App() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      hello
+      <button onClick={playASound}>play</button>
     </div>
   )
+}
+function playASound() {
+  const synth = new Tone.Synth().toDestination();
+
+  //play a middle 'C' for the duration of an 8th note
+  synth.triggerAttackRelease("C4", "8n");
 }
 
 export default App
