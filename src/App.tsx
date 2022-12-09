1672f8b8b9288347630508cd89764d341ecdfabf
rjan <daaka@pop-os.localdomain>
2022-12-09 19:10:47 +0545

    playing a chord

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,20 @@ import * as Tone from 'tone'
 function App() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <button onClick={playASound}>play</button>
+      <button className='px-2 py-1 bg-violet-200 text-violet-600 rounded hover:bg-violet-100 hover:text-violet-400' onClick={playASound}>play</button>
     </div>
   )
 }
 function playASound() {
-  const synth = new Tone.Synth().toDestination();
+  playChord(["C4", "E4", "g4"])
+}
 
-  //play a middle 'C' for the duration of an 8th note
-  synth.triggerAttackRelease("C4", "8n");
+function playChord(chord: string[]) {
+  const polysynth = new Tone.PolySynth().toDestination()
+  for (let idx in chord) {
+    polysynth.triggerAttackRelease(chord[idx], "8n", `+${idx}`)
+  }
+  polysynth.triggerAttackRelease(chord, "4n", `+${chord.length}`)
 }
 
 export default App
